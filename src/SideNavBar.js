9d3aca1198b5802af43e6084719ebf7e7b8d92bb
Alexina Coullandreau <mozart38@gmail.com>
2021-08-02 10:26:22 +0200

    Adding display section name on nav bar button hover

--- src/SideNavBar.js
+++ src/SideNavBar.js
@@ -15,6 +15,14 @@ class SideNavBar extends React.Component {
 		this.setState({ selected: selectedSection });
     };
 
+   handleItemHover = (e) => {
+   	var targetElement = e.target.nextSibling
+   	if (e.type === "mouseenter") {
+   		targetElement.style.display = "inline-block"
+   	} else {
+   		targetElement.style.display = "none"
+   	}
+   } 
 
 	handleItemClick = (e) => {
 		var targetSection = e.target.name
@@ -23,31 +31,39 @@ class SideNavBar extends React.Component {
 
 	render () {
 		return (
-			<div className="nav-bar">
-				{
-					React.Children.toArray(
-						Object.keys(this.props.config.sections).map((item, i) => {
-							var target = this.props.config.sections[item]
-							var backgroundColor = "#16141266";
-							if (this.props.selection === target) {
-								backgroundColor = "#EC1A2B";
-							}
-							return (
-								<React.Fragment>
-									<button 
-										className="nav-bar-button"
-										id ={`nav-bar-button-${item}`} 
-										name={target}
-										style = {{backgroundColor:backgroundColor}} 
-										onClick={this.handleItemClick}
-									>
-									</button>
-								</React.Fragment>
-							)
-						})
-					)
-				}
-			</div>
+			<React.Fragment>
+				<div className="NavBar">
+					{
+						React.Children.toArray(
+							Object.keys(this.props.config.sections).map((item, i) => {
+								var target = this.props.config.sections[item]
+								var backgroundColor = "#16141266";
+								if (this.props.selection === target) {
+									backgroundColor = "#EC1A2B";
+								}
+								return (
+									<React.Fragment>
+										<button 
+											className="NavBarButton"
+											id = {`nav-bar-button-${item}`} 
+											name={target}
+											item = {item}
+											style = {{backgroundColor:backgroundColor}} 
+											onClick={this.handleItemClick}
+											onMouseEnter={this.handleItemHover}
+											onMouseLeave={this.handleItemHover}
+										>
+										</button>
+										<div id={`nav-bar-text-${item}`}  className="NavMenuExpand" onMouseLeave={this.handleItemHover}>
+											<p >{this.props.config.visuals[target]["title"]}</p>
+										</div>
+									</React.Fragment>
+								)
+							})
+						)
+					}
+				</div>
+			</React.Fragment>
 		);
 
 	}
